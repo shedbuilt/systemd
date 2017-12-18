271b55cceb49712801ee326a6b11b049409c96a7
Auston Stewart <auston@shedbuilt.net>
2017-12-18 11:44:13 -1000

    Redacting post-install script

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-if ${SHED_ISUPGRADE} ; then
-    true
-    # This procedure can't be performed without interrupting the script.
-    # Disabled for now.
-    # systemctl daemon-reload
-    # systemctl start multi-user.target
-else
-    systemd-machine-id-setup
-fi
+systemd-machine-id-setup
