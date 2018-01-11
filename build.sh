2cd640965fd0104b3d27b29088ea423d12dc1c22
Auston Stewart <auston@shedbuilt.net>
2018-01-11 02:17:44 -0500

    Updating to version 236, using ninja and meson

--- build.sh
+++ build.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-./configure --prefix=/usr            \
-            --sysconfdir=/etc        \
-            --localstatedir=/var     \
-            --with-rootprefix=       \
-            --with-rootlibdir=/lib   \
-            --enable-split-usr       \
-            --disable-firstboot      \
-            --disable-ldconfig       \
-            --disable-sysusers       \
-            --disable-manpages       \
-            --with-default-dnssec=no \
-            --docdir=/usr/share/doc/systemd-234
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
+# Remove tests incompatible with chroot
+sed '179,223d' -i src/resolve/meson.build
+# Remove unneeded render group
+sed -i 's/GROUP="render", //' rules/50-udev-default.rules.in
+# Build in a separate directory
+mkdir -v build
+cd build
+LANG=en_US.UTF-8                   \
+meson --prefix=/usr                \
+      --sysconfdir=/etc            \
+      --localstatedir=/var         \
+      -Dblkid=true                 \
+      -Dbuildtype=release          \
+      -Ddefault-dnssec=no          \
+      -Dfirstboot=false            \
+      -Dinstall-tests=false        \
+      -Dkill-path=/bin/kill        \
+      -Dkmod-path=/bin/kmod        \
+      -Dldconfig=false             \
+      -Dmount-path=/bin/mount      \
+      -Drootprefix=                \
+      -Drootlibdir=/lib            \
+      -Dsplit-usr=true             \
+      -Dsulogin-path=/sbin/sulogin \
+      -Dsysusers=false             \
+      -Dumount-path=/bin/umount    \
+      -Db_lto=false                \
+      -Dman=false                  \
+      .. || return 1
+LANG=en_US.UTF-8 NINJAJOBS=$SHED_NUMJOBS ninja || return 1
+LANG=en_US.UTF-8 DESTDIR="$SHED_FAKEROOT" ninja install || return 1
 rm -rfv ${SHED_FAKEROOT}/usr/lib/rpm
 mkdir -v ${SHED_FAKEROOT}/sbin
 for tool in runlevel reboot shutdown poweroff halt telinit; do
-    ln -sfv ../bin/systemctl ${SHED_FAKEROOT}/sbin/${tool}
+    ln -sfv ../bin/systemctl "${SHED_FAKEROOT}/sbin/${tool}"
 done
-ln -sfv ../lib/systemd/systemd ${SHED_FAKEROOT}/sbin/init
+ln -sfv ../lib/systemd/systemd "${SHED_FAKEROOT}/sbin/init"
--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-systemd-machine-id-setup
+# If needed, created the machine-id file used by systemd-journald
+if [ ! -e /etc/machine-id ]; then
+    systemd-machine-id-setup
+fi
+# If needed, install a script to allow unprivileged user logins without systemd-logind
+if [ ! -e /lib/systemd/systemd-user-sessions ]; then
+    install -v -Dm755 "${SHED_CONTRIBDIR}/systemd-user-sessions" "${SHED_FAKEROOT}/lib/systemd/systemd-user-sessions"
+fi
