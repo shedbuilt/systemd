084f01bf9b404f399ddbc6333a2e6fe801fde7ec
Auston Stewart <auston@shedbuilt.net>
2018-03-25 12:45:07 -1000

    Patching compilation issues with util-linux 2.32

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Remove unneeded render group
-sed -i 's/GROUP="render", //' rules/50-udev-default.rules.in
-# Remove tests broken in chroot (REMOVE FOR LATER SYSTEMD)
-sed '178,222d' -i src/resolve/meson.build
+sed -i 's/GROUP="render", //' rules/50-udev-default.rules.in || exit 1
+# Remove tests broken in chroot (REMOVE FOR LATER THAN v237)
+sed '178,222d' -i src/resolve/meson.build || exit 1
+# Patch include issue with util-linux 2.32  (REMOVE FOR LATER THAN v238)
+patch -Np1 -i "${SHED_PATCHDIR}/systemd-v237_util-linux-2.32.part1.patch" &&
+patch -Np1 -i "${SHED_PATCHDIR}/systemd-v237_util-linux-2.32.part2.patch" &&
+patch -Np1 -i "${SHED_PATCHDIR}/systemd-v237_util-linux-2.32.part3.patch" || exit 1
 # Build in a separate directory
-mkdir -v build
-cd build
+mkdir -v build &&
+cd build &&
 LANG=en_US.UTF-8                   \
 meson --prefix=/usr                \
       --sysconfdir=/etc            \
@@ -30,11 +34,11 @@ meson --prefix=/usr                \
       .. &&
 LANG=en_US.UTF-8 NINJAJOBS=$SHED_NUMJOBS ninja &&
 LANG=en_US.UTF-8 DESTDIR="$SHED_FAKEROOT" ninja install || exit 1
-rm -rfv ${SHED_FAKEROOT}/usr/lib/rpm
-mkdir -v ${SHED_FAKEROOT}/sbin
+rm -rfv "${SHED_FAKEROOT}/usr/lib/rpm" &&
+mkdir -v "${SHED_FAKEROOT}/sbin" || exit 1
 for SHEDPKG_TOOL in runlevel reboot shutdown poweroff halt telinit; do
-    ln -sfv ../bin/systemctl "${SHED_FAKEROOT}/sbin/${SHEDPKG_TOOL}"
+    ln -sfv ../bin/systemctl "${SHED_FAKEROOT}/sbin/${SHEDPKG_TOOL}" || exit 1
 done
-ln -sfv ../lib/systemd/systemd "${SHED_FAKEROOT}/sbin/init"
+ln -sfv ../lib/systemd/systemd "${SHED_FAKEROOT}/sbin/init" || exit 1
 # Install an LFS script to allow unprivileged user logins without systemd-logind
-install -v -Dm755 "${SHED_CONTRIBDIR}/systemd-user-sessions" "${SHED_FAKEROOT}/lib/systemd/systemd-user-sessions"
+install -v -Dm755 "${SHED_CONTRIBDIR}/systemd-user-sessions" "${SHED_FAKEROOT}/lib/systemd/systemd-user-sessions" || exit 1
