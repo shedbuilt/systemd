700a02147645015e1874319eeb01ebdd292632a2
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:28:46 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ sed -i 's/GROUP="render", //' rules/50-udev-default.rules.in || exit 1
 # Remove tests broken in chroot (REMOVE FOR LATER THAN v237)
 sed '178,222d' -i src/resolve/meson.build || exit 1
 # Patch include issue with util-linux 2.32  (REMOVE FOR LATER THAN v238)
-patch -Np1 -i "${SHED_PATCHDIR}/systemd-v237_util-linux-2.32.part1.patch" &&
-patch -Np1 -i "${SHED_PATCHDIR}/systemd-v237_util-linux-2.32.part2.patch" &&
-patch -Np1 -i "${SHED_PATCHDIR}/systemd-v237_util-linux-2.32.part3.patch" || exit 1
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/systemd-v237_util-linux-2.32.part1.patch" &&
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/systemd-v237_util-linux-2.32.part2.patch" &&
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/systemd-v237_util-linux-2.32.part3.patch" || exit 1
 # Build in a separate directory
 mkdir -v build &&
 cd build &&
@@ -32,13 +32,13 @@ meson --prefix=/usr                \
       -Db_lto=false                \
       -Dman=false                  \
       .. &&
-LANG=en_US.UTF-8 NINJAJOBS=$SHED_NUMJOBS ninja &&
-LANG=en_US.UTF-8 DESTDIR="$SHED_FAKEROOT" ninja install || exit 1
-rm -rfv "${SHED_FAKEROOT}/usr/lib/rpm" &&
-mkdir -v "${SHED_FAKEROOT}/sbin" || exit 1
+LANG=en_US.UTF-8 NINJAJOBS=$SHED_NUM_JOBS ninja &&
+LANG=en_US.UTF-8 DESTDIR="$SHED_FAKE_ROOT" ninja install || exit 1
+rm -rfv "${SHED_FAKE_ROOT}/usr/lib/rpm" &&
+mkdir -v "${SHED_FAKE_ROOT}/sbin" || exit 1
 for SHEDPKG_TOOL in runlevel reboot shutdown poweroff halt telinit; do
-    ln -sfv ../bin/systemctl "${SHED_FAKEROOT}/sbin/${SHEDPKG_TOOL}" || exit 1
+    ln -sfv ../bin/systemctl "${SHED_FAKE_ROOT}/sbin/${SHEDPKG_TOOL}" || exit 1
 done
-ln -sfv ../lib/systemd/systemd "${SHED_FAKEROOT}/sbin/init" || exit 1
+ln -sfv ../lib/systemd/systemd "${SHED_FAKE_ROOT}/sbin/init" || exit 1
 # Install an LFS script to allow unprivileged user logins without systemd-logind
-install -v -Dm755 "${SHED_CONTRIBDIR}/systemd-user-sessions" "${SHED_FAKEROOT}/lib/systemd/systemd-user-sessions" || exit 1
+install -v -Dm755 "${SHED_PKG_CONTRIB_DIR}/systemd-user-sessions" "${SHED_FAKE_ROOT}/lib/systemd/systemd-user-sessions" || exit 1
