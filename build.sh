b8958abb8a477286278cd0dd59452e22a23862d6
Auston Stewart <auston@shedbuilt.net>
2018-04-16 07:42:58 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -33,12 +33,17 @@ meson --prefix=/usr                \
       -Dman=false                  \
       .. &&
 LANG=en_US.UTF-8 NINJAJOBS=$SHED_NUM_JOBS ninja &&
-LANG=en_US.UTF-8 DESTDIR="$SHED_FAKE_ROOT" ninja install || exit 1
+LANG=en_US.UTF-8 DESTDIR="$SHED_FAKE_ROOT" ninja install &&
 rm -rfv "${SHED_FAKE_ROOT}/usr/lib/rpm" &&
 mkdir -v "${SHED_FAKE_ROOT}/sbin" || exit 1
 for SHEDPKG_TOOL in runlevel reboot shutdown poweroff halt telinit; do
     ln -sfv ../bin/systemctl "${SHED_FAKE_ROOT}/sbin/${SHEDPKG_TOOL}" || exit 1
 done
-ln -sfv ../lib/systemd/systemd "${SHED_FAKE_ROOT}/sbin/init" || exit 1
+ln -sfv ../lib/systemd/systemd "${SHED_FAKE_ROOT}/sbin/init" &&
 # Install an LFS script to allow unprivileged user logins without systemd-logind
-install -v -Dm755 "${SHED_PKG_CONTRIB_DIR}/systemd-user-sessions" "${SHED_FAKE_ROOT}/lib/systemd/systemd-user-sessions" || exit 1
+install -v -Dm755 "${SHED_PKG_CONTRIB_DIR}/systemd-user-sessions" "${SHED_FAKE_ROOT}/lib/systemd/systemd-user-sessions" &&
+# Default network config (Eth0, DHCP, systemd-resolved)
+install -v -Dm644 "${SHED_PKG_CONTRIB_DIR}/systemd/network/10-eth0-dhcp.network" "${SHED_FAKE_ROOT}/usr/share/defaults/etc/systemd/network/10-eth0-dhcp.network" &&
+# Sysctl config
+install -v -Dm644 "${SHED_PKG_CONTRIB_DIR}/sysctl.d/99-sysctl.conf" "${SHED_FAKE_ROOT}/usr/share/defaults/etc/sysctl.d/99-sysctl.conf" &&
+install -v -m644 "${SHED_PKG_CONTRIB_DIR}/sysctl.d/20-quiet-printk.conf" "${SHED_FAKE_ROOT}/usr/share/defaults/etc/sysctl.d"
